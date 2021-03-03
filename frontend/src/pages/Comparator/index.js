1c04826d5c23d0ac255d63c5cd5c8b8284249707
gustavocioccari <gustavocioccari@gmail.com>
2021-03-03 08:11:17 -0300

    Init comparator

---
+++ frontend/src/pages/Comparator/index.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react'
+import api from '../../services/api'
+
+export default function ComparePlans(){
+  return(
+    <div>
+      Hello Comparator!
+    </div>
+  )
+}
